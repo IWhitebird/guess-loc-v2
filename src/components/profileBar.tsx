d43c0a558926d3c01327b8e1f2a60e4752c2539a
Alpesh Pote <108137169+Xeonen24@users.noreply.github.com>
2024-01-19 15:44:01 +0530

    fixed profile bar misplace

--- src/components/profileBar.tsx
+++ src/components/profileBar.tsx
@@ -18,24 +18,22 @@ const Dashboard = () => {
   }
 
   return (
-    <div className='pt-5 transition-all duration-300 ease-in-out'>
-      <div className='flex gap-3 bg-[#ffffffa3] p-2 px-4 rounded-2xl z-50 scale-[0.7] cursor-pointer' onClick={dropdownHandle}>
+    <div className='pt-5 transition-all duration-300 ease-in-out relative'>
+      <div className='flex gap-3 bg-[#ffffffa3] p-1 px-2 h-1/2 rounded-2xl z-50 cursor-pointer' onClick={dropdownHandle}>
         <img
           className='rounded-full'
           src={loggedIN?.user?.user_metadata?.avatar_url ? loggedIN?.user?.user_metadata?.avatar_url : `https://api.dicebear.com/6.x/personas/svg?seed=${loggedIN?.user?.user_metadata?.full_name}`}
           alt="profile"
-          width="100"
-          height="100"
         />
         <div className='flex items-center justify-center'>
-          <p className='text-4xl font-bold'>{loggedIN.user.user_metadata.full_name.split(' ')[0]}</p>
-          <IoMdArrowDropdown className={`duration-200 text-5xl mt-2`}
+          <p className='text-2xl font-bold'>{loggedIN.user.user_metadata.full_name.split(' ')[0]}</p>
+          <IoMdArrowDropdown className={`duration-200 text-3xl mt-1`}
             style={{ transform: dropdown ? 'rotate(180deg)' : 'rotate(0deg)' }}
           />
         </div>
       </div>
       <div className={`transition-all ease-in-out duration-300 z-0 ${dropdown ? 'top-32 z-50' : 'top-9 invisible opacity-0'}
-     absolute p-4 right-32 w-[213px] rounded-lg shadow-lg bg-[rgba(255,255,255,0.8)] backdrop-blur-lg`} >
+     absolute p-4 right-0 w-full rounded-lg shadow-lg bg-[rgba(255,255,255,0.8)] backdrop-blur-lg`} >
         <div className='p-2 flex flex-col gap-5'>
           <p className='text-2xl'>Max: </p>
           <Link to='/mode'>
