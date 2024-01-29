a7bca57b3a5fe1e16e82ad2e3c0d87f3d2a5c5ad
Shreyas Patange <115157819+IWhitebird@users.noreply.github.com>
2024-01-29 13:27:21 +0530

    Changes in chatttoo

--- src/supabase/Routes/RoomRoutes.ts
+++ src/supabase/Routes/RoomRoutes.ts
@@ -19,13 +19,29 @@ export async function sendMessage(roomId : string , message : string , ...args :
 }
 
 export async function updateRoom(roomId : string , message : string , ...args : any[])  {
-    await supabase.from('custom_room').update({
-        room_chat: [...args[0].room_chat, {
-            chatter_id: args[1],
-            chatter_name: args[2],
-            chatter_image: args[3],
-            chatter_message: message,
-            chatter_time: new Date().toLocaleTimeString()
-        }] as any
-    }).match({ room_id: roomId })
+
+    const msg = {
+        chatter_id: args[1],
+        chatter_name: args[2],
+        chatter_image: args[3],
+        chatter_message: message,
+        chatter_time: new Date().toLocaleTimeString()
+    }
+
+    // await supabase.from('custom_room').update({
+    //     room_chat: [...args[0].room_chat, {
+    //         chatter_id: args[1],
+    //         chatter_name: args[2],
+    //         chatter_image: args[3],
+    //         chatter_message: message,
+    //         chatter_time: new Date().toLocaleTimeString()
+    //     }] as any
+    // }).match({ room_id: roomId })
+
+    const a = await supabase.rpc('append_array_to_uuid_row', {
+        row_id : roomId,
+        new_values : [msg]
+    })
+    console.log(a)
+
 }
